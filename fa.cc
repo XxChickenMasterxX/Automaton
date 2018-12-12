844c9d28225e0477b41f14eb5b16129927de1b15
Eloi Leroux <eloi.leroux1802@gmail.com>
2018-12-12 10:20:01 +0100

    yolo

--- fa.cc
+++ fa.cc
@@ -2,30 +2,34 @@
 #include <fstream>
 
 void test(){
-	fa::Automaton automaton;
-    automaton.addState(0);
-    automaton.addState(1);
-    automaton.addState(2); 
-    automaton.addState(3);
-    automaton.addState(4);
+	fa::Automaton fa;
+    fa.addState(0);
+	fa.setStateInitial(0);
+	fa.addState(1);
+	fa.addState(2);
+	fa.addState(3);
+	fa.setStateFinal(3);
+	fa.addState(4);
+	fa.setStateFinal(4);
+	fa.addState(5);
+	
+	fa.addTransition(0,'a',1);
+	fa.addTransition(0,'b',2);
+	fa.addTransition(1,'a',3);
+	fa.addTransition(1,'b',2);
+	fa.addTransition(2,'b',4);
+	fa.addTransition(2,'a',1);
+	fa.addTransition(3,'a',4);
+	fa.addTransition(3,'b',5);
+	fa.addTransition(4,'a',3);
+	fa.addTransition(4,'b',5);
+	fa.addTransition(5,'a',5);
+	fa.addTransition(5,'b',5);
+	
+	fa::Automaton fm;
+	fm = fm.createMinimalMoore(fa);
 
-	automaton.setStateInitial(0);
-	automaton.setStateInitial(1);
-
-	automaton.setStateFinal(1);
-	automaton.setStateFinal(4);
-
-	automaton.addTransition(0,'a',1);
-	automaton.addTransition(0,'a',2);
-	automaton.addTransition(0,'a',3);
-	automaton.addTransition(1,'b',3);
-	automaton.addTransition(2,'a',3);
-	automaton.addTransition(2,'b',4);
-	automaton.addTransition(3,'a',3);
-	automaton.addTransition(3,'b',4);
-	automaton.addTransition(4,'a',4);
-
-	automaton.prettyPrint(std::cout);
+	fm.prettyPrint(std::cout);
 }
 
 int main(){
@@ -39,13 +43,14 @@ int main(){
 	automaton.setStateFinal(2);
 	automaton.addTransition(1,'a',2);
 	automaton.addTransition(2,'b',1);
-	
+	test();
+	/*
 	std::ofstream fichier("GrapheTest.dot", std::ios::out | std::ios::trunc);
 	if(fichier)
 	{
 	    automaton.dotPrint(fichier);
 	}else
-	    std::cerr << "fail open ! " << std::endl;
+	    std::cerr << "fail open ! " << std::endl;*/
 	
 	// pour convertir le .dot en fichier image
 	// dans le terminal :
