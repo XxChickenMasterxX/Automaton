a6ed40d5a155f8d28c32317800832438d2a98065
Eloi Leroux <eloi.leroux1802@gmail.com>
2018-12-21 19:30:50 +0100

    create an application in 3h

--- fa.cc
+++ fa.cc
@@ -1,62 +1,263 @@
 #include "Automaton.h"
-#include <fstream>
 
-void test(){
-	fa::Automaton fa;
-    fa.addState(0);
-	fa.setStateInitial(0);
-	fa.addState(1);
-	fa.addState(2);
-	fa.addState(3);
-	fa.setStateFinal(3);
-	fa.addState(4);
-	fa.setStateFinal(4);
-	fa.addState(5);
-	
-	fa.addTransition(0,'a',1);
-	fa.addTransition(0,'b',2);
-	fa.addTransition(1,'a',3);
-	fa.addTransition(1,'b',2);
-	fa.addTransition(2,'b',4);
-	fa.addTransition(2,'a',1);
-	fa.addTransition(3,'a',4);
-	fa.addTransition(3,'b',5);
-	fa.addTransition(4,'a',3);
-	fa.addTransition(4,'b',5);
-	fa.addTransition(5,'a',5);
-	fa.addTransition(5,'b',5);
-	
-	fa::Automaton fm;
-	fm = fm.createMinimalMoore(fa);
+//-----------------------------------------
+//-------------- Dot File -----------------
+//-----------------------------------------
+/*
+    permet de crée un fichier .dot représentant l'automat
+*/
+void toDot(fa::Automaton fa){ 
+    std::ofstream fichier("YourAutomaton.dot", std::ios::out | std::ios::trunc);
+	if(fichier)
+	{
+	    fa.dotPrint(fichier);
+	    std::cerr << "dot creation succed" << std::endl;
+	}else
+	    std::cerr << "fail create YourAutomaton.dot file " << std::endl;
+	// pour convertir le .dot en fichier image
+	// dans le terminal :
+	//dot -Tpng -oGrapheTest.png GrapheTest.dot
+}
 
-	fm.prettyPrint(std::cout);
+//-----------------------------------------
+//-------------- Manipulation -------------
+//-----------------------------------------
+void manipulateState(fa::Automaton *fa){
+     int state;
+    bool back=false;
+    int choix =-1;
+    while(!back){
+        std::cout << "\n --- Manipulate State --- \n\tAdd lambda state press 1\n\tAdd initial state press 2\n\tAdd final state press 3\n\tRemove state press 4\n\tCount state press 5\n\tBack press 0"<< std::endl;
+        while(!(std::cin >> choix)){
+            std::cout << "\nerror, you must enter an Integer!\n\tYour choice : ";
+        }
+        switch(choix){
+            case 1:
+                
+                 std::cout << " --- Add State --- \nTo add one state write the name of the state (the name must be an Integer)\n\tState name : ";
+                while(!(std::cin >> state)){
+                    std::cout << "error, the name must be an Integer\n\tState name : ";
+                }
+                fa->addState(state);
+                break;
+            case 2:
+               
+                 std::cout << " --- Add Initial State--- \nTo create or set an initial state write the name of the state (the name must be an Integer)\n\tState name : ";
+                while(!(std::cin >> state)){
+                    std::cout << "error, the name must be an Integer\n\tState name : ";
+                }
+                if(!fa->hasState(state)){
+                    fa->addState(state);
+                }
+                fa->setStateInitial(state);
+                break;
+            case 3:
+                
+                 std::cout << " --- Add Final State--- \nTo create or set a final state write the name of the state (the name must be an Integer)\n\tState name : ";
+                while(!(std::cin >> state)){
+                    std::cout << "error, the name must be an Integer\n\tState name : ";
+                }
+                if(!fa->hasState(state)){
+                    fa->addState(state);
+                }
+                fa->setStateFinal(state);
+                break;
+            case 4:
+                
+                 std::cout << " --- Remove State--- \nTo remove state (and his potential transition) write the name of the state (the name must be an Integer)\n\tState name : ";
+                while(!(std::cin >> state)){
+                    std::cout << "error, the name must be an Integer\n\tState name : ";
+                }
+                if(fa->hasState(state)){
+                    fa->removeState(state);
+                }
+                break;
+            case 5:
+                
+                 std::cout << " --- Count State--- \n\tTo count number of state(s)" << std::endl;
+                std::cout << "number of state(s) " << fa->countStates() << std::endl;
+                break;
+            case 0:
+                back = true;
+                break;
+            default:
+                std::cout << "\nYour choice might be not implemented yet" << std::endl;
+                break;
+        }
+    }
+}
+void manipulateAutomaton(fa::Automaton *fa){
+    char alpha;
+    int to;
+    int from;
+    bool back=false;
+    bool verif=false;
+    int choix =-1;
+    while(!back){
+        std::cout << "\n --- Manipulate Automaton --- \n\tManipulate state press 1\n\tAdd one transition press 2\n\tRemove one transition press 3\n\tMake your automaton determinitic press 4\n\tMake your automaton complete press 5\n\tMake the complement of your automaton press 6\n\tBack press 0"<< std::endl;
+        while(!(std::cin >> choix)){
+            std::cout << "\nerror, you must enter an Integer!\n\tYour choice : ";
+        }
+        switch(choix){
+            case 1:
+                
+                 manipulateState(fa);
+                break;
+            case 2:
+                
+                 std::cout << "\n --- Add Transition --- \nTo add one transition write the name of the from state, then the character of your transition and then th name of the to state \n\tfrom State name : ";
+                 while(!(std::cin >> from)){
+                    std::cout << "\nerror, the name must be an Integer\n\tfrom State name : ";
+                }
+                 do{
+                    while(!(std::cin >> alpha)){
+                        std::cout << "\nerror, the character must be a character\n\tcharacter : ";
+                    }
+                    if(isprint(alpha) || alpha == '\0'){
+                        std::cout << "\nerror, the character must be printable\n\tcharacter : ";
+                        verif=true;
+                    }
+                 }while(verif);
+                 while(!(std::cin >> to)){
+                    std::cout << "\nerror, the name must be an Integer\n\tto State name : ";
+                }
+                fa->addTransition(to,alpha,from);
+                break;
+            case 3:
+                
+                 std::cout << "\n --- Remove Transition --- \nTo remove one transition write the name of the from state, then the character of your transition and then th name of the to state \n\tfrom State name : ";
+                 while(!(std::cin >> from)){
+                    std::cout << "\nerror, the name must be an Integer\n\tfrom State name : ";
+                }
+                 do{
+                    while(!(std::cin >> alpha)){
+                        std::cout << "\nerror, the character must be a character\n\tcharacter : ";
+                    }
+                    if(isprint(alpha) || alpha == '\0'){
+                        std::cout << "\nerror, the character must be printable\n\tcharacter : ";
+                        verif=true;
+                    }
+                 }while(verif);
+                 while(!(std::cin >> to)){
+                    std::cout << "\nerror, the name must be an Integer\n\tto State name : ";
+                }
+                if(fa->hasTransition(to,alpha,from))
+                    fa->removeTransition(to,alpha,from);
+                break;
+            case 5:
+                std::cout << "\n --- Make Deterministic ---" << std::endl;
+                *fa=fa::Automaton::createDeterministic(*fa);
+                break;
+            case 6:
+                std::cout << "\n --- Make Complete --- " << std::endl;
+                fa->makeComplete();
+                break;
+            case 7:
+                std::cout << "\n --- Make Complement --- " << std::endl;
+                fa->makeComplement();
+                break;
+            case 0:
+                back = true;
+                break;
+            default:
+                std::cout << "\nYour choice might be not implemented yet" << std::endl;
+                break;
+        }
+    }
+}
+
+//-----------------------------------------
+//------------------ Check ----------------
+//-----------------------------------------
+void checkAutomaton(fa::Automaton fa){
+//is language empty
+//is included in 
+    bool back=false;
+    int state;
+    int choix =-1;
+    while(!back){
+        std::cout << "\n --- Check Automaton --- \n\tIs state initial ? press 1\n\tIs state final ? press 2\n\tIs a determinitic automaton press 3\n\tIs automaton complete press 4\n\tIs an empty language press 5\n\tIs included in (not implemented yet)\n\tBack press 0"<< std::endl;
+        while(!(std::cin >> choix)){
+            std::cout << "\nerror, you must enter an Integer!\n\tYour choice : ";
+        }
+        switch(choix){
+            case 1:
+                 std::cout << "\n --- Is state initial ? ---\n\t Enter the name of the state:" << std::endl;
+                 
+                 while(!(std::cin >> state)){
+                    std::cout << "\nerror, the name must be an Integer\n\t State name : ";
+                }
+                std::cout << "The state "<< state << (fa.isStateInitial(state) ? " is" : " is not") << " initial" << std::endl; 
+                break;
+            case 2:
+                std::cout << "\n --- Is state final ? ---\n\t Enter the name of the state:" << std::endl;
+                
+                while(!(std::cin >> state)){
+                    std::cout << "\nerror, the name must be an Integer\n\t State name : ";
+                }
+                std::cout << "The state "<< state << (fa.isStateFinal(state) ? " is" : " is not") << " final" << std::endl; 
+                break;
+            case 3:
+                std::cout << "\n --- Deterministic Automaton ? ---" << std::endl;
+                std::cout << "The automaton " << (fa.isDeterministic() ? " is" : " is not") << " deterministic" << std::endl;
+                break;
+            case 4:
+                std::cout << "\n --- Complete Automaton ? ---" << std::endl;
+                std::cout << "The automaton " << (fa.isComplete() ? " is" : " is not") << " complete" << std::endl;
+                break;
+            case 5:
+                 std::cout << "\n --- Empty Language ? ---" << std::endl;
+                std::cout << "The Language " << (fa.isLanguageEmpty() ? " is" : " is not") << " empty" << std::endl;
+                break;
+            case 0:
+                back = true;
+                break;
+            default:
+                std::cout << "\nYour choice might be not implemented yet" << std::endl;
+                break;
+        }
+
+    }
 }
 
 int main(){
-   
-    fa::Automaton automaton;
-
-  	automaton.addState(1); 
-    automaton.addState(2);
-
-	automaton.setStateInitial(1);
-	automaton.setStateFinal(2);
-	automaton.addTransition(1,'a',2);
-	automaton.addTransition(2,'b',1);
-	test();
-	/*
-	std::ofstream fichier("GrapheTest.dot", std::ios::out | std::ios::trunc);
-	if(fichier)
-	{
-	    automaton.dotPrint(fichier);
-	}else
-	    std::cerr << "fail open ! " << std::endl;*/
+    fa::Automaton fa;
+    bool exit=false;
+    int choix =-1;
+    while(!exit){
+        std::cout << "\n --- Automaton Project --- \n\tManipulate automaton press 1\n\tCheck your automaton press 2\n\tPrint your automaton in terminal press 3\n\tCreate .dot file press 4\n\tClear automaton (not implemented)\n\tExit press 0"<< std::endl;
+        std::cin >> choix;
+        switch(choix){
+            case 1:
+                manipulateAutomaton(&fa);
+                break;
+            case 2:
+                checkAutomaton(fa);
+                break;
+            case 3:
+                std::cout << "\n --- print Automaton in terminal --- " << std::endl; 
+                fa.prettyPrint(std::cout);
+                break;
+            case 4:
+                char verifDot;
+                std::cout << "\n --- Create .dot File --- \n\t Caution : the File is name YourAutomaton.dot\n\tlocate in automaton/build\n\tIt erase file with the same name\n\tContinue ? y or n" << std::endl;
+                while(!(std::cin >> verifDot)){
+                    std::cout << "You must press 'y' for yes or 'n' for no!" << std::endl;
+                }
+                
+                if(verifDot=='y'){
+                    toDot(fa);
+                }else
+                    std::cout << "dot creation cancelled" << std::endl;
+                break;
+            case 0:
+                exit = true;
+                break;
+            default:
+                std::cout << "\nYour choice might be not implemented yet" << std::endl;
+                break;
+        }
+    }
 	
-	// pour convertir le .dot en fichier image
-	// dans le terminal :
-	
-	//dot -Tpng -oGrapheTest.png GrapheTest.dot
-	
-
     return 0;
 }
